7e17330e2f8662dbc22afa98fa2b8a8ebc5dd97f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Form1.vb
@@ -0,0 +1,34 @@
+Imports Microsoft.VisualBasic
+Imports System.Windows.Forms
+Imports DevExpress.XtraRichEdit.API.Native
+Imports DevExpress.XtraRichEdit.Utils
+Imports System.Drawing
+
+Namespace RichEditTableCellFixedSize
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+
+			GenerateSampleTable()
+		End Sub
+
+		Private Sub GenerateSampleTable()
+			Dim table As Table = richEditControl1.Document.Tables.Add(richEditControl1.Document.CaretPosition, 3, 3)
+
+			' Major adjustments
+			table.TableLayout = TableLayoutType.Fixed
+
+			table.PreferredWidthType = WidthType.Fixed
+			table.PreferredWidth = Units.InchesToDocumentsF(3f)
+
+			table.Rows(1).HeightType = HeightType.Exact
+			table.Rows(1).Height = Units.InchesToDocumentsF(0.25f)
+
+			' Additional adjustments
+			richEditControl1.Document.InsertText(table(1, 1).Range.Start, "Fixed Cell")
+			table(1, 1).BackgroundColor = Color.LightBlue
+			table(1, 1).LeftPadding = 0
+		End Sub
+	End Class
+End Namespace
